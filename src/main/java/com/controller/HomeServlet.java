b928853dd55c61d28c662ce788196aa8b55e10a0
2020211001001430-ZhaoWenChen <1194920061@qq.com>
2022-05-04 16:39:59 +0800

    week 9 commit

--- src/main/java/com/controller/HomeServlet.java
+++ src/main/java/com/controller/HomeServlet.java
@@ -6,14 +6,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "HomeServlet" ,value = "/home")
 public class HomeServlet extends HttpServlet {
+    private String message;
+    public void init(){
+        message = "Hello World";
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        System.out.println("i am in HelloServlet__>doGet()");
+
+        //Hello
+        PrintWriter out=response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>"+message+"</h1>");
+        out.println("</body></html>");
+
+
         //when u run or user click Home form menu - method is get
         request.getRequestDispatcher("WEB-INF/views/index.jsp").forward(request,response);
         //copy index.jsp inside WEB-INF/views/
